5a7c49e7c6fed79dfca956a89c15c6bbeee940aa
Karolis Ramanauskas <kramanauskas112@gmail.com>
2023-04-23 11:54:52 -0700

    switched up design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,10 +7,28 @@ import { Paper,
    Grid,
   Button,
   IconButton,
+  Divider,
   Stack,
+  useThemeProps,
  } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+
+function Column(props) {
+  
+  return (
+    <Paper elevation={6} sx={{ display: 'flex', height: '100%', width: '100%', padding: '1rem', margin: '1rem', minWidth: '25rem' }}>
+      <Stack sx={{ display: 'flex', width: '100%', alignItems: 'center' }}>
+      <div style={{ marginRight: 'auto'}}>{props.title}</div>
+      <Divider flexItem style={{ margin: '0.5rem 0rem' }}/>
+      <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+        {props.children}
+      </div>
+      </Stack>
+    </Paper>
+  )
+}
+
 
 function App() {
 
@@ -30,45 +48,24 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Paper square sx={{ width: '100vw', height: '100vh'}}>
-          <Paper elevation={12} sx={{ minHeight: '5rem'}}>  
-              <Grid container direction={'row'} columnSpacing={3} alignItems='center' justifyContent='center' minHeight={'5rem'}>
-                <Grid item><Button variant='outlined'>Intro</Button></Grid>
-                <Grid item><Button variant='outlined'>Experience</Button></Grid>
-                <Grid item><Button variant='outlined'>Projects</Button></Grid>
-                <Grid item><Button variant='outlined'>About Me</Button></Grid>
+          <Stack direction='row'>
+            <Column title='About Me'>
+              <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}><Avatar src={profile_pic} sx={{ width: '15rem', height: '15rem', border: '2px solid white' }}/> </div>
+              <p>
+                I'm Karolis and I'm currently working as a Software Engineer at Boeing 
+                in a branch of the company called Flight Test & Evaluation. This website isn't flashy,
+                but I hope I can display what I have done in the past in a simple and effective way.
+              </p>
+              <Grid container direction = {'row'} alignItems='center' columnSpacing={1} justifyContent='center'>
+                <Grid item ><a href='https://www.linkedin.com/in/kramanau/' target='_blank' rel="noreferrer"><IconButton><LinkedInIcon/></IconButton></a></Grid>
+                <Grid item><a href='https://github.com/kramanau' target='_blank' rel="noreferrer"><IconButton><GitHubIcon/></IconButton></a></Grid>
               </Grid>
-          </Paper>
-          <Stack>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '2rem'}}>
-            <Paper elevation={6} sx={{ display: 'flex', maxWidth: '60rem', minWidth: '60rem', maxHeight: '20rem', padding: '2rem' }}>
-              <Avatar src={profile_pic} sx={{ width: '15rem', height: '15rem', border: '2px solid white' }}/>
-              <Grid container direction={'column'} rowGap={0}>
-                <Paper style={{ margin: '2rem', backgroundColor: '#89622e' }} >
-                  <p style={{ padding: '1rem' }}>Hello and welcome to my web portfolio where I can share my experiences and projects
-                  in a more interesting and interactive way. I'm Karolis and I'm currently working as a Software Engineer at Boeing 
-                  in a branch of the company called Flight Test & Evaluation. This website isn't flashy,
-                  but I hope I can display what I have done in the past in a simple and effective way.
-                  </p>
-                </Paper>
-                <Paper style={{ alignSelf: 'center', width: '9rem', backgroundColor: '#89622e' }}>
-                  <Grid container direction = {'row'} alignItems='center' columnSpacing={1} justifyContent='center'>
-                    <Grid item ><a href='https://www.linkedin.com/in/kramanau/' target='_blank' rel="noreferrer"><IconButton><LinkedInIcon/></IconButton></a></Grid>
-                    <Grid item><a href='https://github.com/kramanau' target='_blank' rel="noreferrer"><IconButton><GitHubIcon/></IconButton></a></Grid>
-                  </Grid>
-                </Paper>
-              </Grid>
-            </Paper>
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '2rem'}}>
-              <Paper elevation={6} sx={{ display: 'flex', maxWidth: '60rem', minWidth: '60rem', maxHeight: '20rem', padding: '2rem' }}>
-
-              </Paper>
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '2rem'}}>
-              <Paper elevation={6} sx={{ display: 'flex', maxWidth: '60rem', minWidth: '60rem', maxHeight: '20rem', padding: '2rem' }}>
-
-              </Paper>
-            </div>
+              <p>
+                On my free time I like to
+              </p>
+            </Column>
+           <Column title='Experience'></Column>
+           <Column title='Projects'></Column>
           </Stack>
         </Paper>
       </ThemeProvider>
